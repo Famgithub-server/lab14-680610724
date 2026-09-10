5fb418dc9f45deb7d427537539b6e38c9ad0396d
Fxm <fam0637186002@gmail.com>
2026-09-10 17:14:37 +0700

    update

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,11 +1,19 @@
 import UserRegisterCard from "../components/UserRegisterCard";
-import type { Registrant } from "../libs/Registrant";
+import { loadTasks } from "../libs/Storage";
+
 
 export default function DashboardPage() {
+  const registrants = loadTasks();
+
   return (
-    <div className="container mt-4">
-      <h2>Dashboard</h2>
-      {/* Conditional Rendering + Render Component */}
-    </div>
+    <>
+      <div className="container mt-4">
+        <h2>Dashboard</h2>
+      </div>
+
+      {registrants.map((registrant) => (
+        <UserRegisterCard key={registrant.id} registrant={registrant} />
+      ))}
+    </>
   );
 }
